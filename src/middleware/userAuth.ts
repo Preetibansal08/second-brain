5dc24426b56aa6ebdc4b65159165d807db9a6088
tushar <tusharkashayp9368@gmail.com>
2026-03-22 11:33:34 +0530

    change build command

--- src/middleware/userAuth.ts
+++ src/middleware/userAuth.ts
@@ -1,53 +1,58 @@
 import type { Request, Response, NextFunction } from "express";
-import jwt, { decode } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/env.js";
-
 import UserModul from "../models/UserModels.js";
 
 export const userAuth = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   try {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({
+      res.status(401).json({
         message: "Unauthorized user",
       });
+      return;
     }
 
     const token = authHeader.split(" ")[1];
 
-    if(!token){
-        return res.json({
-            massage:"please login"
-        })
+    if (!token) {
+      res.status(401).json({
+        message: "Please login",
+      });
+      return;
     }
 
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET) as { id: string };
 
-     if(!decode){
-        return res.json({
-            massage:"Unauth user"
-        })
+    if (!decoded) {
+      res.status(401).json({
+        message: "Unauthorized user",
+      });
+      return;
     }
-    //@ts-ignore
+
     const user = await UserModul.findById(decoded.id);
 
     if (!user) {
-      return res.status(401).json({
+      res.status(401).json({
         message: "User not found",
       });
+      return;
     }
+
 
     req.userId = user._id.toString();
 
-    next();
+    return next(); // ✅ IMPORTANT
   } catch (error) {
-    return res.status(401).json({
+    res.status(401).json({
       message: "Invalid or expired token",
     });
+    return;
   }
 };
